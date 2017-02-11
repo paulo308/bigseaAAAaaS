ebc087f258ad48047e31964bf5767598e0f898bd
emorais <eduardommorais@bitbucket.org>
2017-02-11 14:14:37 -0200

    Added script to use google authenticator

---
+++ testsite/main.js
@@ -0,0 +1,22 @@
+// BigSea 
+// Google authenticator
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+  
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  $('#is_logged').text('logged');
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+  $('#is_logged').text('not logged');
+}
