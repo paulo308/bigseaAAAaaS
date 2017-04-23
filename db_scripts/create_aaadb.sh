9c8be66d161d967f05d28e9553cc2ed0d699e137
eduardo <eduardo.morais@gmail.com>
2017-04-23 13:12:01 -0300

    Create dump folder
    This folder contains mongodb dump files, such that it is possible to
    persist database on docker host.

--- db_scripts/create_aaadb.sh
+++ db_scripts/create_aaadb.sh
@@ -1,8 +1,12 @@
 
 sudo rm -rf data/*
+sudo rm -rf dump/*
 #sed -i 's/mongod\ --port\ 27017.*/mongod\ --port\ 27017\ --dbpath\ \/data\/db/' docker-compose.yml
 
 # criacao de usuario
 docker-compose run --entrypoint python3 --name aaa_manager aaa_manager -m db_scripts.create_mongo_user
+
+# Gerar dump do banco de dados
+docker exec mongo_test mongodump
 
 #sed -i 's/mongod\ --port\ 27017.*/mongod\ --port\ 27017\ --clusterAuthMode\ x509\ --sslMode\ requireSSL\ --sslPEMKeyFile\ \/certs\/mongo_crt\.pem --sslCAFile\ \/certs\/root_ca\.pem\ --dbpath\ \/data\/db/' docker-compose.yml
