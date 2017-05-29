8869ee853ee5371299dfb72c8c15dca9343cda95
Paulo Silva <paulo308@users.noreply.github.com>
2017-05-29 14:51:42 +0100

    Google Sign In
    Working. Integration with accounts is needed. Development of functions to receive parameters from google and create response is needed.

--- aaa_manager/pages/templates/static/js/home.js
+++ aaa_manager/pages/templates/static/js/home.js
@@ -65,3 +65,27 @@ function login(){
         }
     });
 }
+
+
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	var id_token = googleUser.getAuthResponse().id_token;
+
+	//console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	//console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	console.log('ID_TOKEN: ' + id_token);
+	$('#is_logged').text('logged');
+<!--  window.location.replace("http://eubrabigsea.dei.uc.pt/postlogin.html");
+-->
+}
+
+
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
