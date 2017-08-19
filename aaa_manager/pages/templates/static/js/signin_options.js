68d0790cf96a83ba0b72ab61313d48f435494086
Paulo Silva <paulo308@users.noreply.github.com>
2017-08-19 15:54:40 +0100

    Update javascript for signin options
    forgot pwd and resend email

--- aaa_manager/pages/templates/static/js/signin_options.js
+++ aaa_manager/pages/templates/static/js/signin_options.js
@@ -16,6 +16,18 @@ $(function() {
 	}
     });
 
+   $( "#btn_resend_email" ).click(function() {
+        console.log('clickResendEmail');
+        var f = $('#resendemailform');
+        f.parsley().validate();
+        if (f.parsley().isValid()) {
+                console.log('email and user are valid. proceed to submit form and resend confirmation email');
+                resend_email();
+        } else {
+                console.log('username or email validation failed');
+        }
+    });
+
 });
 
 var view_data;
@@ -38,6 +50,34 @@ function forgot_pw(){
                 $('#error').show();
             } else {
                 msg = "Password reset with success!";
+                $('#error').text('');
+                $('#error').hide();
+                $('#success').text(msg);
+                $('#success').show();
+            }
+        }
+    });
+}
+
+
+function resend_email(){
+    $.ajax({
+        url: '/engine/api/send_email_token',
+        type: 'post',
+        data: {'username': $('#user').val(), 'email': $('#email').val()},
+        success: function (result) {
+            view_data = result;
+            console.log(result);
+            console.log(result['error']);
+            error = result['error'];
+            if (error) {
+                //alert(error);
+                $('#success').text('');
+                $('#success').hide();
+                $('#error').text(error);
+                $('#error').show();
+            } else {
+                msg = "Confirmation email sent!";
                 $('#error').text('');
                 $('#error').hide();
                 $('#success').text(msg);
